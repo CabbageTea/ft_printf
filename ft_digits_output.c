41865ebff092160e504854a7ccc7d78cecb4806e
CabbageTea <glaser.des@gmail.com>
2017-12-29 21:25:26 -0800

    digits

--- ft_digits_output.c
+++ ft_digits_output.c
@@ -6,7 +6,7 @@
 /*   By: dglaser <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/27 19:41:32 by dglaser           #+#    #+#             */
-/*   Updated: 2017/12/29 21:24:54 by dglaser          ###   ########.fr       */
+/*   Updated: 2017/12/29 21:25:22 by dglaser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int				ft_dig_2(long long num, int len, t_con *todo)
 		return (ft_prezeropadding(num, todo));
 	if (todo->minus_flag == 0 && todo->dot_flag == 1 &&
 			todo->precision > todo->min_width
-			&& todo-zero_flag == 0 && todo->space_flag == 1)
+			&& todo->zero_flag == 0 && todo->space_flag == 1)
 		return (ft_prezeropadding(num, todo));
 	if (todo->minus_flag == 0 && todo->precision > ft_intlen(len))
 		len = ft_padding(todo->min_width - todo->precision, *todo);
