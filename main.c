b44ff63e7dff489e9f16e167762d3898ce30f52f
CabbageTea <glaser.des@gmail.com>
2017-12-30 21:05:37 -0800

    holy

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 int main(void)
-{
+/*{
 	char *s1 = "Hello world";
 	char *s2 = "cats are cool";
 
@@ -8,5 +8,12 @@ int main(void)
 	ft_printf("%s", s2);
 
 	while (1);
+	return (0);
+}*/
+{
+	// test->debug = 1;
+	ft_printf("{%10d}", 42);
+	ft_printf("{%-30S}", L"我是一只猫。");
+	ft_printf("%5.5%");
 	return (0);
 }
