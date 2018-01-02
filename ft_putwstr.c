03a9c23036abd3af195d6370ce17566a02ef084f
CabbageTea <glaser.des@gmail.com>
2018-01-02 00:27:15 -0800

    norming

--- ft_putwstr.c
+++ ft_putwstr.c
@@ -6,7 +6,7 @@
 /*   By: dglaser <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/02 00:20:00 by dglaser           #+#    #+#             */
-/*   Updated: 2018/01/02 00:22:46 by dglaser          ###   ########.fr       */
+/*   Updated: 2018/01/02 00:26:43 by dglaser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,18 +49,19 @@ int		ft_wstr_output(t_con *todo, va_list ap, int i, int x)
 	if (y == NULL)
 		return (ft_nullstr(x));
 	if (todo->min_width > 0 && todo->minus_flag == 0)
-		x = ft_padding(todo->min_width - ft_wstrlen(y), *todo);
+	   	x = ft_padding(todo->min_width - ft_wstrlen(y), *todo);
 	if (todo->dot_flag == 1)
 	{
 		while (i++ < todo->precision && y[i] != '\0')
+		{
 			write(1, &y[i], 1);
+		}
 	}
 	else
 	{
-		while (y[i] != '\0')
+		while (y[i++] != '\0')
 		{
 			write(1, &y[i], 1);
-			i++;
 		}
 	}
 	if (todo->min_width > 0 && todo->minus_flag == 1)
